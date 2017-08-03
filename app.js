f14d0e041fc688ca80188a856d743b0a80bd9973
Prabhdeep <ps591990@gmail.com>
2017-08-02 21:36:34 -0500

    update final touches

--- app.js
+++ app.js
@@ -1,88 +1,85 @@
-// var canvas = document.querySelector("canvas");
-// var c = canvas.getContext("2d");
+var canvas = document.querySelector("canvas");
+var c = canvas.getContext("2d");
 
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-// var particleCount = 1000;
+var particleCount = 500;
 
 
-// function LightParticle(x, y, radius, color) {
-//   this.x = x;
-//   this.y = y;
-//   this.radius = radius;
-//   this.color = color;
+function LightParticle(x, y, radius, color) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.color = color;
 
-//   this.update = function() {
-//     this.draw();
-//   };
+  this.update = function() {
+    this.draw();
+  };
 
-//   this.draw = function() {
-//     c.save();
-//     c.beginPath();
-//     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-//     c.shadowColor = this.color;
-//     c.shadowBlur = 15;
-//     c.shadowOffsetX = 0;
-//     c.shadowOffsetY = 0;
-//     c.fillStyle = this.color;
-//     c.fill();
-//     c.closePath();
-//     c.restore();
-//   };
-// }
+  this.draw = function() {
+    c.save();
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
 
-// var lightParticles = [];
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+    c.restore();
+  };
+}
 
-// var timer = 0;
-// var opacity = 1;
-// var speed = 0.0005;
-// var colors = ["#0952BD", "#A5BFF0", "#118CD6", "#1AAEE8", "#F2E8C9"];
+var lightParticles = [];
 
-// var initializeParticles;
+var timer = 0;
+var opacity = 1;
+var speed = 0.0005;
+var colors = ["#0952BD", "#A5BFF0", "#118CD6", "#1AAEE8", "#F2E8C9"];
 
-// (initializeParticles = function() {
-//   for (var i = 0; i < particleCount; i++) {
-//     var randomColorIndex = Math.floor(Math.random() * 6);
-//     var randomRadius = Math.random() * 2;
+var initializeParticles;
 
-//     // Ensure particles are spawned past screen width and height so
-//     // there will be no missing stars when rotating canvas
-//     var x = Math.random() * (canvas.width + 200) - (canvas.width + 200) / 2;
-//     var y = Math.random() * (canvas.width + 200) - (canvas.width + 200) / 2;
-//     lightParticles.push(
-//       new LightParticle(x, y, randomRadius, colors[randomColorIndex])
-//     );
-//   }
-// })();
+(initializeParticles = function() {
+  for (var i = 0; i < particleCount; i++) {
+    var randomColorIndex = Math.floor(Math.random() * 6);
+    var randomRadius = Math.random() * 2;
 
-// function animate() {
-//   window.requestAnimationFrame(animate);
+    // Ensure particles are spawned past screen width and height so
+    // there will be no missing stars when rotating canvas
+    var x = Math.random() * (canvas.width + 200) - (canvas.width + 200) / 2;
+    var y = Math.random() * (canvas.width + 200) - (canvas.width + 200) / 2;
+    lightParticles.push(
+      new LightParticle(x, y, randomRadius, colors[randomColorIndex])
+    );
+  }
+})();
 
-//   c.save();
+function animate() {
+  window.requestAnimationFrame(animate);
 
-//   // Ease back to the original opacity
-//   var originalOpacity = 1;
-//   opacity += (originalOpacity - opacity) * 0.01;
-//   c.fillStyle = "rgba(18, 18, 18, " + opacity + ")";
+  c.save();
 
-//   // Ease back to the original speed
-//   var originalSpeed = 0.001;
-//   speed += (originalSpeed - speed) * 0.01;
-//   timer += speed;
+  // Ease back to the original opacity
+  var originalOpacity = 1;
+  opacity += (originalOpacity - opacity) * 0.01;
+  c.fillStyle = "rgba(18, 18, 18, " + opacity + ")";
 
-//   c.fillRect(0, 0, canvas.width, canvas.height);
-//   c.translate(canvas.width / 2, canvas.height / 2);
-//   c.rotate(timer);
+  // Ease back to the original speed
+  var originalSpeed = 0.001;
+  speed += (originalSpeed - speed) * 0.01;
+  timer += speed;
 
-//   for (var i = 0; i < lightParticles.length; i++) {
-//     lightParticles[i].update();
-//   }
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  c.translate(canvas.width / 2, canvas.height / 2);
+  c.rotate(timer);
 
-//   c.restore();
-// }
+  for (var i = 0; i < lightParticles.length; i++) {
+    lightParticles[i].update();
+  }
 
-// animate();
+  c.restore();
+}
+
+animate();
 
 
 
